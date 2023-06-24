8dade332a3e59e4e8b6b98e3a02afeaaae8b252b
MartinDevox2811 <manhdaokhacmanh@gmail.com>
2023-06-24 13:52:01 +0700

    feat: create page

---
+++ go-server/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	uploadDir = "./uploads"
+	chunkSize = 50 * 1024 * 1024 // 50MB
+)
+
+func main() {
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+	r.POST("/v1/file/upload", handleUpload)
+	r.Run(":8081")
+}
+
+func handleUpload(c *gin.Context) {
+	fileHeader, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File upload failed"})
+		return
+	}
+
+	filename := c.PostForm("filename")
+	fileExt := filepath.Ext(filename)
+	fileNameWithoutExt := strings.TrimSuffix(filename, fileExt)
+
+	chunkIndex, _ := strconv.Atoi(c.PostForm("chunkindex"))
+	totalChunks, _ := strconv.Atoi(c.PostForm("totalchunks"))
+
+	// Create path file name
+	chunkPath := filepath.Join(uploadDir, fmt.Sprintf("%s.part%d", fileNameWithoutExt, chunkIndex))
+	out, err := os.Create(chunkPath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating chunk file"})
+		return
+	}
+	defer out.Close()
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
+		return
+	}
+	defer file.Close()
+
+	buffer := make([]byte, chunkSize)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err != io.EOF {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading file"})
+			return
+		}
+
+		if n == 0 {
+			break
+		}
+
+		// Write file buffer to chunk file
+		_, err = out.Write(buffer[:n])
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error writing chunk file"})
+			return
+		}
+	}
+
+	if chunkIndex == totalChunks {
+		// Create a final file to merge all the chunks
+		mergedFilePath := filepath.Join(uploadDir, filename)
+		mergedFile, err := os.Create(mergedFilePath)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating merged file"})
+			return
+		}
+		defer mergedFile.Close()
+
+		for i := 1; i <= totalChunks; i++ {
+			// Find chunk file path
+			chunkPath := filepath.Join(uploadDir, fmt.Sprintf("%s.part%d", fileNameWithoutExt, i))
+			chunkFile, err := os.Open(chunkPath)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening chunk file"})
+				return
+			}
+			defer chunkFile.Close()
+
+			_, err = io.Copy(mergedFile, chunkFile)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error merging chunks"})
+				return
+			}
+
+			os.Remove(chunkPath) // Remove chunk file after merging
+		}
+
+		// Rename the merged file to the original filename
+		err = os.Rename(mergedFilePath, filepath.Join(uploadDir, filename))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error renaming the merged file"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "File upload successful"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": "Chunk upload successful"})
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,107 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState, ChangeEvent } from "react";
+
+const UploadFile = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [progress, setProgress] = useState<number>(0);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      return;
+    }
+
+    const chunkSize = 1024 * 1024 * 50; // 1MB
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    let uploadedChunks = 0;
+
+    for (let i = 0; i < totalChunks; i++) {
+      const start = i * chunkSize;
+      const end = Math.min(file.size, start + chunkSize);
+      const chunk = file.slice(start, end);
+
+      const formData = new FormData();
+      formData.append("file", chunk);
+      formData.append("fileName", file.name);
+      formData.append("chunkIndex", String(i + 1));
+      formData.append("totalChunk", String(totalChunks));
+      formData.append("chunkSize", String(chunkSize));
+      formData.append(
+        "data",
+        JSON.stringify({
+          productId: "e19caaac-f8fc-4428-b722-3de5d533ce09",
+          productTitle: "martin test",
+          style: {
+            background: "black",
+          },
+          psdUrl: "https://example.com/imageurl",
+          thumbnailUrl: "https://example.com/thumbnailurl",
+          name: "fileName",
+          psdProductId: "08345783-b536-4e2c-9bef-7b42d03e0a82",
+          userId: "ba2b3f1a-a9a9-41b3-b4fc-10b2ae28319d",
+          variant: {
+            color: "Blue",
+            size: "Medium",
+            price: 15.99,
+          },
+          vendor: "GG",
+          title: "this is file title",
+        })
+      );
+
+      try {
+        const response = await fetch("http://localhost:8081/v1/file/upload", {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODc1OTg0OTQsImlhdCI6MTY4NzUxMjA5NCwidXNlcklEIjoiYmEyYjNmMWEtYTlhOS00MWIzLWI0ZmMtMTBiMmFlMjgzMTlkIiwiZW1haWwiOiJtYW5oZGtAZ29kZ3JvdXAuY28iLCJyb2xlIjoiU1VQRVJfQURNSU4ifQ.EMXyiZHxcvYNrRhBK71MHcnxDECf_GnFX4XPlErzZS4`,
+          },
+          onUploadProgress: (progressEvent: {
+            loaded: number;
+            total: number;
+          }) => {
+            const progressPercent = Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            );
+            setProgress(progressPercent);
+          },
+        });
+
+        if (response.ok) {
+          uploadedChunks++;
+          setProgress((uploadedChunks / totalChunks) * 100);
+        } else {
+          console.error("Upload error:", response.statusText);
+          // Handle upload error
+        }
+      } catch (error) {
+        console.error("Upload error:", error);
+        // Handle upload error
+      }
+    }
+
+    if (uploadedChunks === totalChunks) {
+      // All chunks uploaded successfully
+      // Handle completion
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload} disabled={!file}>
+        Upload
+      </button>
+      {progress > 0 && <progress value={progress} max="100" />}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default UploadFile;
