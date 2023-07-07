9daf82810af3a88c8e3320924c9be17e1c86bb3b
MartinDevox2811 <manhdaokhacmanh@gmail.com>
2023-07-07 07:30:16 +0700

    fix: update download file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,25 +51,22 @@ const UploadFile = () => {
       );
 
       try {
-        const response = await fetch(
-          "https://nas.godmerch.com/v1/file/upload",
-          {
-            method: "POST",
-            body: formData,
-            headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODg3MjM3MzQsImlhdCI6MTY4ODYzNzMzNCwidXNlcklEIjoiYmEyYjNmMWEtYTlhOS00MWIzLWI0ZmMtMTBiMmFlMjgzMTlkIiwiZW1haWwiOiJtYW5oZGtAZ29kZ3JvdXAuY28iLCJyb2xlIjoiU1VQRVJfQURNSU4ifQ.DuwTisR5B7G_vMPQnNXWDwhkniZ64fwv1VJS2MLa8e0`,
-            },
-            onUploadProgress: (progressEvent: {
-              loaded: number;
-              total: number;
-            }) => {
-              const progressPercent = Math.round(
-                (progressEvent.loaded / progressEvent.total) * 100
-              );
-              setProgress(progressPercent);
-            },
-          }
-        );
+        const response = await fetch("https://host/v1/file/upload", {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer `,
+          },
+          onUploadProgress: (progressEvent: {
+            loaded: number;
+            total: number;
+          }) => {
+            const progressPercent = Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            );
+            setProgress(progressPercent);
+          },
+        });
 
         if (response.ok) {
           uploadedChunks++;
